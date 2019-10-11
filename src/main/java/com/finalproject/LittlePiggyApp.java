e3bf42a5df1f13684d4831843bb49ceb026a5d3f
Florian <florincioi123@gmail.com>
2019-10-11 21:28:35 +0300

    Task 3 setup Spring boot Application

---
+++ src/main/java/com/finalproject/LittlePiggyApp.java
@@ -0,0 +1,12 @@
+package com.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LittlePiggyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LittlePiggyApp.class, args);
+    }
+}
