f9f10e72ae34be9fbabd376da13c95d1ba70f2d5
Florian <florincioi123@gmail.com>
2019-10-16 20:03:52 +0300

    Task 13 Configure Spring Security

---
+++ src/main/java/com/finalproject/securityconfig/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.finalproject.securityconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/","/index").permitAll()
+                .and().authorizeRequests().antMatchers("/static/").permitAll()
+                .and().authorizeRequests().antMatchers("/login", "logout").permitAll()
+                .and().authorizeRequests().antMatchers("/admin").authenticated()
+                .and().formLogin().loginPage("/login").defaultSuccessUrl("/").permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable(); //H2 console runs inside a frame so we need to disable it from spring security.
+    }
+
+
+}
